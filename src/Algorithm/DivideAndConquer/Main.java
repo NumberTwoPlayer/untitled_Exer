608e9d6118cb188a4829a135e37a49fae46fb096
刘梹晨 <3394427855@qq.com>
2022-09-06 20:04:05 +0800

    Changes

--- src/Algorithm/DivideAndConquer/Main.java
+++ src/Algorithm/DivideAndConquer/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
         HanoiTower(5,'A','B','C');
     }
-
     public static void HanoiTower(int num, char a, char b, char c) {
         if (num == 1) {
             System.out.println("第1个盘从:" + a + " --> " + c);
